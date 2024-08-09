1efe4151be0fd74fc8a7387a1e14be235b47fa03
Lucas Jacquemin <lucas.jacquemin2002@gmail.com>
2024-08-09 17:51:01 +0200

    help

--- srcs/wordpress/create_wp.sh
+++ srcs/wordpress/create_wp.sh
@@ -1,25 +1,65 @@
 #!/bin/sh
 
+BOLD="\e[1m"
+RESET="\e[0m"
+
+# Text colors
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+WHITE="\e[37m"
+
 #check if wp-config.php exist
-if [ -f ./wp-config.php ]
-then
-	echo "wordpress already downloaded"
-else
-	#Create wp-config with WP-CLI
-	wp config create \
-		--dbname=$db_name \
-		--dbuser=$db_user \
-		--dbpass=$db_pwd \
-		--dbhost=localhost \
-		--dbprefix=wp_
+#if [ -f ./wp-config.php ]
+#then
+#	echo "wordpress already downloaded"
+#else
 
-	#Install WP
-	wp core install \
-		--url=$DOMAIN_NAME \
-		--title=$WP_TITLE \
-		--admin_user=$WP_ADMIN_USR \
-		--admin_password=$WP_ADMIN_PWD \
-		--admin_email=$WP_ADMIN_EMAIL
-fi
+echo "DB_NAME: $db_name"
+echo "DB_USER: $db_user"
+echo "DB_PWD: $db_pwd"
+echo "DOMAIN_NAME: $DOMAIN_NAME"
+echo "WP_TITLE: $WP_TITLE"
+echo "WP_ADMIN_USR: $WP_ADMIN_USR"
+echo "WP_ADMIN_PWD: $WP_ADMIN_PWD"
+echo "WP_ADMIN_EMAIL: $WP_ADMIN_EMAIL"
 
-exec "$@"
+echo "${BOLD}${GREEN}making html dir${RESET}"
+mkdir -p /var/www/html && cd /var/www/html
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+#Create wp-config with WP-CLI
+
+wp --info
+
+echo "${BOLD}${GREEN}WP downloaded${RESET}"
+wp core download --allow-root
+
+echo "${BOLD}Creating wp-config $db_name ${RESET}"
+wp config create --allow-root \
+	--dbname=$db_name \
+	--dbuser=$db_user \
+	--dbpass=$db_pwd \
+	--dbhost=localhost \
+	--dbprefix=wp_
+
+
+echo "${BOLD}${GREEN}WP-config created${RESET}"
+
+#Install WP
+wp core install --allow-root \
+	--url=$DOMAIN_NAME \
+	--title=$WP_TITLE \
+	--admin_user=$WP_ADMIN_USR \
+	--admin_password=$WP_ADMIN_PWD \
+	--admin_email=$WP_ADMIN_EMAIL
+	
+/usr/sbin/php-fpm7.3 -F
+#fi
+
+#exec "$@"
